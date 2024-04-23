d7ee2763bef6ab9a496c39388b97222f5a075aa5
Ravenhash <33248257+Ravenhash@users.noreply.github.com>
2024-04-23 08:02:45 +0200

    Version 1.2

---
+++ partials/top-bar.php
@@ -0,0 +1,41 @@
+<div class="kingster-top-bar">
+                <div class="kingster-top-bar-background"></div>
+                <div class="kingster-top-bar-container kingster-container ">
+                <div class="kingster-top-bar-container-inner clearfix">
+              <div class="kingster-top-bar-left kingster-item-pdlr">
+                <i class="fa fa-envelope-open-o" id="i_fd84_0"></i>
+                <a
+                  href="mailto:enquiries@togi.ac.za?subject=Website Enquiry&body=Name:%0DSurname:%0DContact No:%0D"
+                  >enquiries@togi.ac.za</a
+                >
+                <i class="fa fa-phone" id="i_fd84_1"></i> +27 21 829 7015
+              </div>
+              <div class="kingster-top-bar-right kingster-item-pdlr">
+                <ul
+                  id="kingster-top-bar-menu"
+                  class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu"
+                >
+                  <li
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6684 kingster-normal-menu"
+                  >
+                    <a href="https://togi.ac.za/contact-us/">Contact Us</a>
+                  </li>
+                  <li
+                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-7406 kingster-normal-menu"
+                  >
+                    <a href="https://togi.ac.za/category/latest-news/"
+                      >Latest News</a
+                    >
+                  </li>
+                </ul>
+                <div class="kingster-top-bar-right-social"></div>
+                <a
+                  class="kingster-top-bar-right-button"
+                  href="http://eepurl.com/ij-rpH"
+                  target="_blank"
+                  >Subscribe to TOGI's Newsletter</a
+                >
+              </div>
+            </div>
+                </div>
+            </div>
